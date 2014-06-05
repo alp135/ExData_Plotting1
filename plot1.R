df47e290bbc43820b519dc6f12c2b6b455ebee50
alp135 <anoukpost@gmail.com>
2014-06-05 21:53:08 +0200

    deletions

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ data<-read.table('household_power_consumption.txt',header=TRUE,sep=";",na.string
 dataSmall<-subset(data,data$Date=='1/2/2007' | data$Date=='2/2/2007')
 
 # Save graph as png file
-png('/Users/anoukpost/Documents/Coursera/Exploratory_Data_Analysis/plot1.png',width=480,height=480)
+png('/Users/anoukpost/Documents/Coursera/Exploratory_Data_Analysis/figure/plot1.png',width=480,height=480)
 hist(dataSmall$Global_active_power,col='red',
      xlab='Global Active Power (kilowatts)',main='Global Active Power')
 
